405d6bd17a15004ff2ae89c1fc0b8e6ee6d02993
sbx_user1051 <sbx_user1051@169.254.63.93>
2024-05-12 18:49:59 +0000

    Sun May 12 18:49:59 UTC 2024

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,6 +6,7 @@ from lib.solutions.CHK.checkout_solution import checkout
 @pytest.mark.parametrize("bucket, expected", [
     ("AAABBCDE", 250),
     ("AAABBCDEE", 260),
+    ("AAABBCDEE", 260),
     ("AAAAABBCDEE", 330),
     ("aBCD", -1),
     ("ABBCD", 130),
@@ -16,5 +17,6 @@ def test_checkout_success(bucket, expected):
     actual = checkout(bucket)
 
     assert actual == expected
+
 
 
