b08c562e50dadc266989057f1e822dff69dc789b
sbx_user1051 <sbx_user1051@169.254.63.93>
2024-05-12 17:52:54 +0000

    Sun May 12 17:52:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,37 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+
+SPECIAL_A = {
+    3: 130
+}
+
+SPECIAL_B = {
+    2: 45
+}
+
+PRODUCTS = {
+    "A": {"regular": 50, "special": {"cnt": 3, "price": 130}},
+    "B": {"regular": 30, "special": {"cnt": 2, "price": 45}},
+    "C": {"regular": 20},
+    "D": {"regular": 15}
+}
+
+
+def checkout(skus: str) -> int:
+    total = 0
+    bucket = {}
+    for item in skus:
+        if item not in PRODUCTS:
+            return -1
+        num = bucket.get(item, 0) + 1
+        bucket[item] = num
+
+    for item, num in bucket.items():
+        special = PRODUCTS.get(item, {}).get("special")
+        if special:
+            cnt = special["cnt"]
+            price = special["price"]
+            if item > cnt:
+                discount_cnt = item // cnt
+                total += discount_cnt *
